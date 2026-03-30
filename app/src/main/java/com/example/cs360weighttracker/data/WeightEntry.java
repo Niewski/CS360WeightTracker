5af611ca6ac1ae1ac4e27a499fa9af21d9551d18
Jacob Wisniewski <jacobawisniewski@gmail.com>
2026-03-30 15:01:45 -0400

    implement comparable on weightentry

--- app/src/main/java/com/example/cs360weighttracker/data/WeightEntry.java
+++ app/src/main/java/com/example/cs360weighttracker/data/WeightEntry.java
@@ -1,6 +1,8 @@
 package com.example.cs360weighttracker.data;
 
-public class WeightEntry {
+import java.util.Objects;
+
+public class WeightEntry implements Comparable<WeightEntry> {
     public int id;
     public String date;
     public double weight;
@@ -9,5 +11,23 @@ public class WeightEntry {
         this.id = id;
         this.date = date;
         this.weight = weight;
+    }
+
+    @Override
+    public int compareTo(WeightEntry other) {
+        return this.date.compareTo(other.date);
+    }
+
+    @Override
+    public boolean equals(Object o) {
+        if (this == o) return true;
+        if (o == null || getClass() != o.getClass()) return false;
+        WeightEntry that = (WeightEntry) o;
+        return id == that.id;
+    }
+
+    @Override
+    public int hashCode() {
+        return Objects.hash(id);
     }
 }
